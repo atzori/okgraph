0c2e75320c495f9474e27409714d17ca8fad0a18
Emanuele Mameli <emanuelemameli@gmail.com>
2019-08-03 11:19:05 +0200

    Running expreriments with a different order to improve it

--- okgraph/task/set_expansion/optimum/experiment/experiments.py
+++ okgraph/task/set_expansion/optimum/experiment/experiments.py
@@ -4,6 +4,7 @@ import numpy
 import numpy as np
 import datetime
 import scipy
+import threading
 
 from dataset_helper import *
 from optimization_core import *
@@ -12,7 +13,6 @@ print(f'Started')
 numpy.seterr(divide='ignore', invalid='ignore')
 
 
-# centroid
 def mean(vector: [numpy.ndarray]):
     """
     Calculate the centroid vector.
@@ -237,38 +237,39 @@ def run_experiments(models: list,
                     lazy_loading: int = 0,
                     verbose: bool = False):
 
+    n=1
     for embeddings_magnitude_model in models:
     
-        if verbose:  
+        if verbose:
             print(f'Loading model: {embeddings_magnitude_model}')
         okg = okgraph.OKgraph(corpus=corpus_file_path, embeddings=embeddings_magnitude_model, lazy_loading=lazy_loading)
-        if verbose:  
+        if verbose:
             print(f'Model: {embeddings_magnitude_model} LOADED')
 
         for ground_truth_name in ground_truths:
             ground_truth = load(ground_truth_name) # this will read the text file
             k_topn = len(ground_truth)
-            if verbose:  
+            if verbose:
                 print(f'Getting the ground_truth and without_not_exists')
             ground_truth = [w.replace(" ", "_") for w in ground_truth]
             ground_truth_without_not_exists = [e for e in ground_truth if e in okg.v]
-            if verbose:  
+            if verbose:
                 print(f'DONE. ground_truth=[{ground_truth}] and without_not_exists=[{ground_truth_without_not_exists}]')
             enable_most_similar_approx = False
             for one_model_name in ["GoogleNews-vectors-negative300", "glove.6B.300d", "glove.840B.300d"]:
                 enable_most_similar_approx = enable_most_similar_approx or (one_model_name in embeddings_magnitude_model)
             sklearn_metric_ap_score_enabled = True  # False can reduce computation time if sklearn not used in the objective_metrics
 
-            if verbose:  
+            if verbose:
                 print(f'Getting the initial_guesses_list')
             seed_sizes = generate_seed_sizes(len(ground_truth_without_not_exists))
-            initial_guesses_list = get_random_lists(ground_truth_without_not_exists, seed_sizes)
-            if verbose:  
+            initial_guesses_list = get_random_lists(ground_truth_without_not_exists, seed_sizes)#[0:1]
+            if verbose:
                 print(f'Done. initial_guesses_list={initial_guesses_list} ')
 
             now = datetime.datetime.now()
 
-            if verbose:  
+            if verbose:
                 print(f'initial_guesses_list length = {len(initial_guesses_list)}')
                 print(f'ground_truth_without_not_exists length = {len(ground_truth_without_not_exists)}')
                 print(f'seed_sizes = {seed_sizes}')
@@ -279,9 +280,9 @@ def run_experiments(models: list,
             total = len(optim_algos) * len(initial_guesses_list) * len(objective_metrics) * k_topn
             tmp = total
 
-            for optim_algo in optim_algos:
-                filename = f'results/res_{optim_algo}_{now.strftime("%Y-%m-%d_%H:%M:%S.%f")[:-3]}.csv'
-                for initial_guesses in initial_guesses_list:
+            for initial_guesses in initial_guesses_list:
+                for optim_algo in optim_algos:
+                    filename = f'results/res_{optim_algo}_{now.strftime("%Y-%m-%d_%H:%M:%S.%f")[:-3]}.csv'
                     for objective_metric in objective_metrics:
 
                         print(f'Computing x0 from [{len(initial_guesses)}] vectors '
@@ -303,8 +304,22 @@ def run_experiments(models: list,
                             "enable_most_similar_approx": enable_most_similar_approx,
                             "verbose": verbose
                         }
-                        _ = get_optimum(okg, dataset_info, choose_x0, filename, verbose=verbose)
-                        if verbose:  
+                        # _ = get_optimum(okg, dataset_info, choose_x0, filename, verbose=verbose)
+
+                        args = {
+                            "okg": okg, 
+                            "dataset_info": dataset_info, 
+                            "choose_x0_closure": choose_x0, 
+                            "filename": filename, 
+                            "verbose": verbose
+                        }
+                        globals()['thread_list'].append(threading.Thread(name="T" + str(n), target=get_optimum, args=(args,)))
+                        thread_list = globals()['thread_list']
+                        globals()['thread_list'][-1].start()
+                        print(f'\nRunning {len(thread_list)} threads')
+
+
+                        if verbose:
                             print(f'\n\n')
                         else:
                             print(f'ENDED ONE OF [{len(initial_guesses)}] vectors '
@@ -312,63 +327,48 @@ def run_experiments(models: list,
                                 f'by using : [{objective_metric}] >>> [{filename}]')
                         tmp -= 1
 
-        if verbose:  
+        if verbose:
             print(f'Optimization ended')
             
         print(f'filename = {filename}')
 
 
-# run_experiments(models=[embeddings_magnitude_modelGN],
-#                 ground_truths=[load('usa_states')],
-#                 optim_algos=['powell', 'nelder-mead', 'BFGS', 'Newton-CG', 'CG', 'TNC', 'COBYLA', 'SLSQP', 'dogleg', 'trust-ncg'],
-#                 objective_metrics=['AP@k', 'MAP', 'sklearn_metric_ap_score_weighted', 'sklearn_metric_ap_score_macro'],
 
 
 def experiment_t1(args_dict):
-    one_optim_algo=args_dict['one_optim_algo']
+    optim_algos_list=args_dict['optim_algos_list']
     ground_truths=args_dict['ground_truths']
     models=args_dict['models']
     lazy_loading=args_dict['lazy_loading']
-    print("Starting experiment... [one_optim_algo: " + str(one_optim_algo) + " ]" )
+    print(f"Starting experiments... ["+str(optim_algos_list)+"] " )
     ## (k, n) make an expertiment with a list of "k" elements for "n" times
     run_experiments(models=models,
                     ground_truths=ground_truths,
-                    optim_algos=[one_optim_algo],
+                    optim_algos=optim_algos_list,
                     objective_metrics=['AP@k'],
                     lazy_loading=lazy_loading,
                     verbose=False)
-    print("Ended experiment... [one_optim_algo: " + str(one_optim_algo) + " ]" )
-    globals()['tot_threads_running'] = globals()['tot_threads_running'] - 1
-    tot_threads_running = globals()['tot_threads_running']
-    print(F'Running yet {tot_threads_running}/{len(thread_list)} threads')
+    n_threads = len(globals()['thread_list'])
+    print(f"Running [{n_threads}] threads/experiments." )
 
-import threading
 
 print("STARTING")
 print("STARTING")
 print("STARTING")
 print("STARTING")
 # optim_algos_list = ['powell', 'nelder-mead', 'BFGS', 'Newton-CG', 'CG', 'TNC', 'COBYLA', 'SLSQP', 'dogleg', 'trust-ncg']
-optim_algos_list = ['powell', 'BFGS', 'COBYLA']
-globals()['tot_threads_running'] = 0
 thread_list = []
 n=0
-for one_optim_algo in optim_algos_list:
-    n=n+1
-    args = {
-        "one_optim_algo": one_optim_algo,
-        "ground_truths": ['usa_states', 'universe_solar_planets', 'king_of_rome', 'period_7_element'],
-        "models": [embeddings_magnitude_modelGN, embeddings_magnitude_modelGlove6B, embeddings_magnitude_modelGlove840B],
-        "lazy_loading": 0   #  You can pass in an optional lazy_loading argument to the constructor with the value
-                            #   -1 to disable lazy-loading and pre-load all vectors into memory (a la Gensim), 
-                            #   0 (default) to enable lazy-loading with an unbounded in-memory LRU cache, or 
-                            #   an integer greater than zero X to enable lazy-loading with an LRU cache that holds the X most recently used vectors in memory.
-    }
-    thread_list.append(threading.Thread(name="T" + str(n), target=experiment_t1, args=(args,)))
+args = {
+    # "optim_algos_list": ['BFGS', 'COBYLA', 'powell'],#, 'BFGS', 'COBYLA', 'nelder-mead', 'Newton-CG', 'CG'],
+    "optim_algos_list": ['TNC', 'SLSQP', 'dogleg', 'trust-ncg', 'BFGS', 'COBYLA', 'nelder-mead', 'Newton-CG', 'CG', 'powell'],
+    "ground_truths": ['universe_solar_planets', 'king_of_rome', 'period_7_element', 'usa_states'],# 'usa_states', 'universe_solar_planets', 'king_of_rome', 'period_7_element'],
+    "models": [embeddings_magnitude_modelGlove6B],#embeddings_magnitude_modelGN , embeddings_magnitude_modelGlove6B, embeddings_magnitude_modelGlove840B],
+    "lazy_loading": 0   #  You can pass in an optional lazy_loading argument to the constructor with the value
+                        #   -1 to disable lazy-loading and pre-load all vectors into memory (a la Gensim), 
+                        #   0 (default) to enable lazy-loading with an unbounded in-memory LRU cache, or 
+                        #   an integer greater than zero X to enable lazy-loading with an LRU cache that holds the X most recently used vectors in memory.
+}
 
-print(F'Running {len(thread_list)} threads')
+experiment_t1(args)
 
-for t in thread_list:
-    t.start()
-
-globals()['tot_threads_running'] = len(thread_list)
